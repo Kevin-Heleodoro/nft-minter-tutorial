3c6470cdc37cde2b03e5f947098afee89c6ffb8a
Kevin Heleodoro <kevin.heleodoro@gmail.com>
2022-04-28 16:38:07 -0500

    connect pinata to ipfs

---
+++ minter-starter-files/src/utils/pinata.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const axios = require('axios');
+
+const { PINATA_KEY, PINATA_SECRET } = process.env;
+
+export const pinJSONtoIPFS = async (JSONBody) => {
+	const url = `https://api.pinata.cloud/pinning/pinJSONToIPFS`;
+
+	return axios
+		.post(url, JSONBody, {
+			headers: {
+				pinata_api_key: PINATA_KEY,
+				pinata_secret_api_key: PINATA_SECRET,
+			},
+		})
+		.then((res) => {
+			return {
+				success: true,
+				pinataUrl: `https://gateway.pinata.cloud/ipfs/${res.data.IpfsHash}`,
+			};
+		})
+		.catch((error) => {
+			console.log(`Whoops: ${error}`);
+			return {
+				success: false,
+				message: error.message,
+			};
+		});
+};
